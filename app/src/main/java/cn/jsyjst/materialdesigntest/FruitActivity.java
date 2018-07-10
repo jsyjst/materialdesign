131e49b28be595ccdfbff97dc8dd452a5d21fceb
jsyjst <y8588130@163.com>
2018-07-10 16:41:02 +0800

    materdesign

---
+++ app/src/main/java/cn/jsyjst/materialdesigntest/FruitActivity.java
@@ -0,0 +1,47 @@
+package cn.jsyjst.materialdesigntest;
+
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
+
+public class FruitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fruit);
+
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        CollapsingToolbarLayout collapsingToolbar=findViewById(R.id.collapsing_toolbar);
+        TextView fruitContentText=findViewById(R.id.fruit_content_text);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar=getSupportActionBar();
+        if(actionBar!=null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        String fruitContent=generateFruitContent("grape");
+        fruitContentText.setText(fruitContent);
+    }
+    private String generateFruitContent(String fruitName){
+        StringBuilder fruitContent=new StringBuilder();
+        for(int i=0;i<500;i++){
+            fruitContent.append(fruitName);
+        }
+        return fruitContent.toString();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
